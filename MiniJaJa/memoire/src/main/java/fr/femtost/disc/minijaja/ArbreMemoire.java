8385d95e6d42cfce6a68981094ca951121ee76e8
abordy2 <abordy2@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-10-09 14:45:18 +0000

    maj tas

--- MiniJaJa/memoire/src/main/java/fr/femtost/disc/minijaja/ArbreMemoire.java
+++ MiniJaJa/memoire/src/main/java/fr/femtost/disc/minijaja/ArbreMemoire.java
@@ -23,4 +23,36 @@ public class ArbreMemoire {
         return Math.max(profondeurArbre(courant.droit), profondeurArbre(courant.gauche))+1;
     }
 
+    public void libererMemoire(int adresse) {
+        racine.suppressionMemoire(adresse, racine);
+    }
+
+    public NoeudMemoire getNoeudRecursif(int adresse, NoeudMemoire courant) {
+        //System.out.println("Adresse courante: " + courant.adresse+ ", " + courant.gauche + ", " + courant.droit);
+        if(courant.adresse == adresse && courant.gauche == null && courant.droit == null) {
+            return courant;
+        }
+        if (courant.gauche != null) {
+            //System.out.println("adresse courante : " + courant.adresse  + " ,taille gauche: " + courant.gauche.taille + ", adresse: " + adresse);
+            if (courant.adresse + courant.gauche.taille - adresse > 0) {
+                return getNoeudRecursif(adresse, courant.gauche);
+            }
+        }
+        if (courant.droit != null) {
+            //System.out.println("Va à droite");
+            return getNoeudRecursif(adresse, courant.droit);
+        }
+        return null;
+    }
+
+    public NoeudMemoire getNoeud(int adresse) {
+        if (adresse >= racine.taille) {
+            return null;
+        }
+        if (adresse < 0 ) {
+            return null;
+        }
+        return getNoeudRecursif(adresse, racine);
+    }
+
 }
