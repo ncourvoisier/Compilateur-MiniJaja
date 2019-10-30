795d4c6e6bd83d5acd7c53ca4225c4cb7616156b
abordy2 <abordy2@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-10-30 17:02:26 +0000

    fusion des noeuds mémoire disponibles adjacents

--- MiniJaJa/memoire/src/main/java/fr/femtost/disc/minijaja/NoeudMemoire.java
+++ MiniJaJa/memoire/src/main/java/fr/femtost/disc/minijaja/NoeudMemoire.java
@@ -55,16 +55,26 @@ public class NoeudMemoire {
         }
 
         if (droit == null && gauche == null) {
-            NoeudMemoire noeudNouveau = new NoeudMemoire(adresse, tailleAAllouer, false, this);
-            NoeudMemoire noeudDisponible = new NoeudMemoire(adresse+tailleAAllouer, taille - tailleAAllouer, true, this);
-            if (noeudNouveau.taille <= noeudDisponible.taille) {
-                gauche = noeudNouveau;
-                droit = noeudDisponible;
-            } else {
-                noeudNouveau.adresse = adresse + noeudDisponible.taille; // Le noeud mémoire dispo est plus petit que la mémoire alloué, donc il vient à gauche
-                noeudDisponible.adresse = adresse;
-                gauche = noeudDisponible;
-                droit = noeudNouveau;
+
+            NoeudMemoire noeudNouveau;
+
+            if (taille == tailleAAllouer) { // La taille à allouer est exactement celle disponible sur le noeud, pas de création de nouveaux noeuds
+                disponible = false;
+                tailleDisponible = 0;
+                noeudNouveau = this;
+            }
+            else {
+                noeudNouveau = new NoeudMemoire(adresse, tailleAAllouer, false, this);
+                NoeudMemoire noeudDisponible = new NoeudMemoire(adresse + tailleAAllouer, taille - tailleAAllouer, true, this);
+                if (noeudNouveau.taille <= noeudDisponible.taille) {
+                    gauche = noeudNouveau;
+                    droit = noeudDisponible;
+                } else {
+                    noeudNouveau.adresse = adresse + noeudDisponible.taille; // Le noeud mémoire dispo est plus petit que la mémoire alloué, donc il vient à gauche
+                    noeudDisponible.adresse = adresse;
+                    gauche = noeudDisponible;
+                    droit = noeudNouveau;
+                }
             }
             if (parent != null) {  // On lance la propagation à partir du parent, si pas racine
                 parent.propagationTailleDisponile(-noeudNouveau.taille);
@@ -96,14 +106,17 @@ public class NoeudMemoire {
     }
 
     public void suppressionMemoireReccursive(int adresse, NoeudMemoire courant) {
-        //System.out.println("Adresse : " + courant.adresse + ", Disponible : " + Boolean.toString(courant.disponible));
+        System.out.println("Adresse : " + courant.adresse + ", Disponible : " + Boolean.toString(courant.disponible));
         if(courant.adresse == adresse && !courant.disponible) {
             courant.disponible = true;
             courant.tailleDisponible = courant.taille;
-            if (parent != null) {  // On lance la propagation à partir du parent, si pas racine
-                parent.propagationTailleDisponile(courant.taille);
+            if (courant.parent != null) {  // On lance la propagation à partir du parent, si pas racine
+                System.out.println("Alors: ");
+                courant.parent.propagationTailleDisponile(courant.taille);
+                courant.parent.fusionNoeudsDisponibles();
             }
             else { // si racine, propagation directe
+                System.out.println("Ah..");
                 propagationTailleDisponile(courant.taille);
             }
             return;
@@ -122,6 +135,21 @@ public class NoeudMemoire {
 
     }
 
+    public boolean estUneFeuille() {
+        return gauche == null && droit == null;
+    }
 
+    public void fusionNoeudsDisponibles() {
+        if (gauche == null || droit == null) {
+            return;  // Corrupted tree exception ?
+        }
+        if (gauche.estUneFeuille() && gauche.disponible && droit.estUneFeuille() && droit.disponible) {
+            gauche = null;
+            droit = null;
+            if (parent != null) {
+                parent.fusionNoeudsDisponibles();
+            }
+        }
+    }
 
 }
