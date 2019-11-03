2a6ddcc659ff5e3e0a4df949a1fc2fd72c7681b7
agarnie3 <agarnie3@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-11-03 12:03:43 +0000

    Correction des bugs ALAS + Ajout Omega.java. A tester en conditions réelles.

---
+++ ast/src/main/java/fr/femtost/disc/minijaja/ast/expr/Omega.java
@@ -0,0 +1,18 @@
+package fr.femtost.disc.minijaja.ast.expr;
+
+import fr.femtost.disc.minijaja.CompilationCouple;
+import fr.femtost.disc.minijaja.ast.ASTExpr;
+import fr.femtost.disc.minijaja.jcodes.JNil;
+
+public final class Omega extends ASTExpr {
+
+    public String rewrite() {
+        return "";
+    }
+
+    @Override
+    public CompilationCouple compiler(int actual) {
+        return new CompilationCouple(new JNil(), 0);
+    }
+
+}
