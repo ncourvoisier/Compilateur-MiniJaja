92fb34e61968bbaaaf4b6142987611fe344a6c76
abordy2 <abordy2@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-11-03 22:23:28 +0000

    non moins op2

--- MiniJaJa/ast/src/main/java/fr/femtost/disc/minijaja/ast/expr/Not.java
+++ MiniJaJa/ast/src/main/java/fr/femtost/disc/minijaja/ast/expr/Not.java
@@ -2,6 +2,7 @@ package fr.femtost.disc.minijaja.ast.expr;
 
 import fr.femtost.disc.minijaja.CompilationCouple;
 import fr.femtost.disc.minijaja.JCodes;
+import fr.femtost.disc.minijaja.Memoire;
 import fr.femtost.disc.minijaja.ast.ASTExpr;
 import fr.femtost.disc.minijaja.jcode.oper.OpUnaire;
 
@@ -26,5 +27,21 @@ public class Not extends ASTExpr {
     public CompilationCouple compiler(int actual) {
         CompilationCouple e = expr.compiler(actual);
         return new CompilationCouple(JCodes.concatRight(e.jCodes, new OpUnaire(OpUnaire.Operandes.NOT)), e.taille + 1);
+    }
+
+    @Override
+    public void interpreter(Memoire m) {
+
+    }
+
+    @Override
+    public void retirer(Memoire m) {
+
+    }
+
+    @Override
+    public Object eval(Memoire m) {
+        Boolean e = (Boolean) expr.eval(m);
+        return !e;
     }
 }
