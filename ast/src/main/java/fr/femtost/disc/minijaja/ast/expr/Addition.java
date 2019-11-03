e5b2d23ddec0857c162abf725a25081db9e3f921
abordy2 <abordy2@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-11-03 22:23:28 +0000

    non moins op2

--- ast/src/main/java/fr/femtost/disc/minijaja/ast/expr/Addition.java
+++ ast/src/main/java/fr/femtost/disc/minijaja/ast/expr/Addition.java
@@ -2,6 +2,7 @@ package fr.femtost.disc.minijaja.ast.expr;
 
 import fr.femtost.disc.minijaja.CompilationCouple;
 import fr.femtost.disc.minijaja.JCodes;
+import fr.femtost.disc.minijaja.Memoire;
 import fr.femtost.disc.minijaja.ast.ASTExpr;
 import fr.femtost.disc.minijaja.jcode.oper.OpBinaire;
 
@@ -34,4 +35,20 @@ public class Addition extends ASTExpr {
         return new CompilationCouple(JCodes.concatenate(e1.jCodes, JCodes.concatRight(e2.jCodes, new OpBinaire(OpBinaire.Operandes.ADD))), e1.taille + e2.taille + 1);
     }
 
+    @Override
+    public void interpreter(Memoire m) {
+
+    }
+
+    @Override
+    public void retirer(Memoire m) {
+
+    }
+
+    @Override
+    public Object eval(Memoire m) {
+        int e1 = (int) expr1.eval(m);
+        int e2 = (int) expr2.eval(m);
+        return e1 + e2;
+    }
 }
