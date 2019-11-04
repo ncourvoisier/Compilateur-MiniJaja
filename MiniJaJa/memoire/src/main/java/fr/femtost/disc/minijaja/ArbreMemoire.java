676a49fee2e24eb3fc4158447229d8d3f2205f19
abordy2 <abordy2@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-11-04 09:16:02 +0000

    retrait de System.out() de debug

--- MiniJaJa/memoire/src/main/java/fr/femtost/disc/minijaja/ArbreMemoire.java
+++ MiniJaJa/memoire/src/main/java/fr/femtost/disc/minijaja/ArbreMemoire.java
@@ -5,8 +5,7 @@ public class ArbreMemoire {
     private NoeudMemoire racine;
     private int tailleMax;
 
-    public ArbreMemoire(int tailleMax)
-    {
+    public ArbreMemoire(int tailleMax) {
         this.tailleMax = tailleMax;
         this.racine = new NoeudMemoire(0, tailleMax, true, null);
     }
@@ -35,21 +34,15 @@ public class ArbreMemoire {
     }
 
     public NoeudMemoire getNoeudRecursif(int adresse, NoeudMemoire courant) {
-        System.out.println("adresse : " + adresse + ", courant : " + courant.adresse);
-        //System.out.println("Adresse courante: " + courant.adresse+ ", " + courant.gauche + ", " + courant.droit);
         if(courant.adresse == adresse && courant.gauche == null && courant.droit == null) {
             return courant;
         }
         if (courant.gauche != null) {
-            System.out.println("essaye à gauche...");
-            //System.out.println("adresse courante : " + courant.adresse  + " ,taille gauche: " + courant.gauche.taille + ", adresse: " + adresse);
             if (courant.adresse + courant.gauche.taille - adresse > 0) {
-                System.out.println("Va à gauche car : " + courant.adresse + " + " + courant.gauche.taille + " - " + adresse + " > 0");
                 return getNoeudRecursif(adresse, courant.gauche);
             }
         }
         if (courant.droit != null) {
-            System.out.println("Va à droite");
             return getNoeudRecursif(adresse, courant.droit);
         }
         return null;
@@ -57,11 +50,9 @@ public class ArbreMemoire {
 
     public NoeudMemoire getNoeud(int adresse) {
         if (adresse >= tailleMax) {
-            System.out.println(adresse + " >= " + racine.taille);
             return null;
         }
         if (adresse < 0 ) {
-            System.out.println("adresse < 0");
             return null;
         }
         return getNoeudRecursif(adresse, racine);
