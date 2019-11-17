f466317f03a85cd8eac63cadbcb70bdbc27edce9
agarnie3 <agarnie3@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-11-17 20:19:16 +0000

    ajout d'un test SyntaxCHecker

--- ALAS/src/test/java/fr/femtost/disc/minijaja/AppTest.java
+++ ALAS/src/test/java/fr/femtost/disc/minijaja/AppTest.java
@@ -2,7 +2,12 @@ package fr.femtost.disc.minijaja;
 
 import static org.junit.Assert.assertTrue;
 
+import fr.femtost.disc.minijaja.ast.ASTClass;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.StringReader;
 
 /**
  * Unit test for simple App.
@@ -17,4 +22,19 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Test
+    public void TestTotalAST(){
+        String s =" class factorielle{ int x=7; void pause(){ int i;}; int fact(int x){ int retour; if (x==0) {";
+                s+="retour=1; }else{ retour=x*fact(x-1); }; return retour; }; main { int res=fact(x); pause(); // res = 5040 } }";
+        try {
+            ASTClass astClass = new SyntaxChecker(new BufferedReader(new StringReader(s))).S();
+            System.out.println(astClass.rewrite());
+        } catch (Throwable e) {
+    System.out.println("Syntax check failed: " + e.getMessage());
+}
+
+        assertTrue(true);
+    }
+
 }
