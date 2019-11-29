cddff821968b6b181aca6d92480d88cf5ba2123f
ncourvoi <ncourvoi@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-11-29 14:47:31 +0000

    ajout nv ihm + test

--- MiniJaJa/IHM/src/test/java/fr/femtost/disc/minijaja/AppTest.java
+++ MiniJaJa/IHM/src/test/java/fr/femtost/disc/minijaja/AppTest.java
@@ -2,19 +2,36 @@ package fr.femtost.disc.minijaja;
 
 import static org.junit.Assert.assertTrue;
 
+import fr.femtost.disc.minijaja.ast.ASTClass;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+
+    @Test
+    public void TestTotalAST() {
+        String s = " class factorielle{ int x=7; void pause(){ int i;}; int fact(int x){ int retour; if (x==0) {";
+        s += "retour=1; }else{ retour=x*fact(x-1); }; return retour; }; main { int res=fact(x); pause(); // res = 5040 } }";
+        try {
+            ASTClass astClass = new SyntaxChecker(new BufferedReader(new StringReader(s))).S();
+            System.out.println(astClass.rewrite());
+        } catch (Throwable e) {
+            System.out.println("Syntax check failed: " + e.getMessage());
+        }
+
+
     }
 }
