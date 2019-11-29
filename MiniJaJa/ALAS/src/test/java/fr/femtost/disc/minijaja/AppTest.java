3049ce68eca4b1b4609d3042a27ce6b2248e3924
dforel <dforel@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-11-29 14:54:35 +0000

    Ajout tests AST

--- MiniJaJa/ALAS/src/test/java/fr/femtost/disc/minijaja/AppTest.java
+++ MiniJaJa/ALAS/src/test/java/fr/femtost/disc/minijaja/AppTest.java
@@ -1,43 +1,18 @@
 package fr.femtost.disc.minijaja;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import fr.femtost.disc.minijaja.ast.ASTClass;
 import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.StringReader;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-
-    @Test
-    public void TestTotalAST(){
-        String s =" class factorielle{ int x=7; void pause(){ int i;}; int fact(int x){ int retour; if (x==0) {";
-                s+="retour=1; }else{ retour=x*fact(x-1); }; return retour; }; main { int res=fact(x); pause(); // res = 5040 } }";
-        try {
-            ASTClass astClass = new SyntaxChecker(new BufferedReader(new StringReader(s))).S();
-            System.out.println(astClass.rewrite());
-        } catch (Throwable e) {
-    System.out.println("Syntax check failed: " + e.getMessage());
-}
-
-        assertTrue(true);
-    }
+public class AppTest {
 
     @Test
     public void test_astOK1() {
@@ -107,7 +82,7 @@ public class AppTest
 
     @Test(expected = ParseException.class)
     public void test_astKO_WriteConcatenate() throws ParseException {
-        String file = "./minijaja/echec3.mjj";
+        String file = "./minijaja/echec4.mjj";
         try {
             new SyntaxChecker(new BufferedReader(new FileReader(file))).S();
         } catch (FileNotFoundException e) {
@@ -116,10 +91,36 @@ public class AppTest
     }
 
     @Test(expected = ParseException.class)
-    public void test_astKO_TEMP() throws ParseException {
-        String file = "./minijaja/echec3.mjj";
+    public void test_astKO_MainParenthesis() throws ParseException {
+        String file = "./minijaja/echec5.mjj";
         try {
             new SyntaxChecker(new BufferedReader(new FileReader(file))).S();
+        } catch (FileNotFoundException e) {
+            fail("Fichier introuvable");
+        }
+    }
+
+    @Test
+    public void test_rewrite1() {
+        String file = "./minijaja/test.mjj";
+        try {
+            ASTClass cl = new SyntaxChecker(new BufferedReader(new FileReader(file))).S();
+            assertFalse(cl.rewrite().isEmpty());
+        } catch (ParseException e) {
+            fail("Erreur de parsing");
+        } catch (FileNotFoundException e) {
+            fail("Fichier introuvable");
+        }
+    }
+
+    @Test
+    public void test_rewrite2() {
+        String file = "./minijaja/synonymie.mjj";
+        try {
+            ASTClass cl = new SyntaxChecker(new BufferedReader(new FileReader(file))).S();
+            assertFalse(cl.rewrite().isEmpty());
+        } catch (ParseException e) {
+            fail("Erreur de parsing");
         } catch (FileNotFoundException e) {
             fail("Fichier introuvable");
         }
