c34ab2e3a9632c5d4710453d46536ef05e9deb9a
ncourvoi <ncourvoi@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-12-11 15:57:11 +0000

    correction chemin ihm

--- IHM/src/main/java/main/java/sample/Main.java
+++ IHM/src/main/java/main/java/sample/Main.java
@@ -13,10 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        String path = System.getProperty("user.dir") + "/IHM/src/main/resources/sample.fxml";
-        System.out.println(path);
         Parent root = FXMLLoader.load(getClass().getResource("../../../sample/sample.fxml"));
-
         primaryStage.setTitle("Compilateur 5");
         Scene sc = new Scene(root, 995, 660);
         primaryStage.setScene(sc);
