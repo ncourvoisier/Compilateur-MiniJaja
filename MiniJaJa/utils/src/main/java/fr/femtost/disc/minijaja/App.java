6b4a4ec2e81477e2065307a7cab80f4289b52bd9
dforel <dforel@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-12-11 14:23:40 +0000

    Ajout module utils

---
+++ MiniJaJa/utils/src/main/java/fr/femtost/disc/minijaja/App.java
@@ -0,0 +1,13 @@
+package fr.femtost.disc.minijaja;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
