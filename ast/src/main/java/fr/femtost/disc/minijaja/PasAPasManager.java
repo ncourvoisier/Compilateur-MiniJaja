4b9534af81c5dc909519a597362875a91d55a576
ncourvoi <ncourvoi@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-12-18 11:48:20 +0000

    commencement breakpoint

---
+++ ast/src/main/java/fr/femtost/disc/minijaja/PasAPasManager.java
@@ -0,0 +1,30 @@
+package fr.femtost.disc.minijaja;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PasAPasManager {
+
+    private Set<Integer> breakPoints;
+    EtatArret arret;
+    Memoire memoire;
+
+    public PasAPasManager() {
+        this.breakPoints = new HashSet<>();
+    }
+
+    public void setBreakPoints(Set<Integer> breakPoints) {
+        this.breakPoints = breakPoints;
+    }
+
+    public void launch() {
+        int currentIndex;
+        if (arret == null) {
+            currentIndex = 0;
+        }
+        else {
+            currentIndex = arret.getIndex();
+        }
+        arret = arret.getNode().interpreterPasAPas(memoire, currentIndex, breakPoints);
+    }
+}
