6079fd8e854fc59a76a7e98f1651aa332d1740f0
ncourvoi <ncourvoi@4e7c6da9-aeaa-4842-8689-c66528fe4b3d>
2019-12-19 09:46:39 +0000

    nettoyage

--- IHM/src/main/java/main/java/sample/Main.java
+++ IHM/src/main/java/main/java/sample/Main.java
@@ -1,20 +1,16 @@
 package main.java.sample;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import sample.Controller;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("../../../sample/sample.fxml"));
         Parent root = FXMLLoader.load(getClass().getResource("../../../sample.fxml"));
         primaryStage.setTitle("Compilateur 5");
         Scene sc = new Scene(root, 1000, 610);
@@ -22,10 +18,7 @@ public class Main extends Application {
         primaryStage.show();
         final Controller ctl = new Controller();
         ctl.setStage(primaryStage);
-
-
     }
-
 
     public static void main(String[] args) {
         launch(args);
